85820e954ed4a8f2daaf208c1ab4ed81c7bad281
Divakar Pal <31034949+MDivakarPal@users.noreply.github.com>
2020-11-20 14:39:53 +0530

    Add files via upload

---
+++ Learn/index.js
@@ -0,0 +1,238 @@
+function af()
+{
+    document.getElementById('a').style.border='1px solid green';
+}
+function gf()
+{
+    document.getElementById('g').style.border='1px solid green';
+}
+function hf()
+{
+    document.getElementById('h').style.border='1px solid green';
+}
+function bf()
+{
+    document.getElementById('b').style.border='1px solid green';
+}
+function cf()
+{
+    document.getElementById('c').style.border='1px solid green';
+}
+function df()
+{
+    document.getElementById('d').style.border='1px solid green';
+}
+function ef()
+{
+    document.getElementById('e').style.border='1px solid green';
+}
+function ff()
+{
+    document.getElementById('f').style.border='1px solid green';
+}
+// Row 2
+function if1()
+{
+    document.getElementById('i').style.border='1px solid green';
+}
+function jf()
+{
+    document.getElementById('j').style.border='1px solid green';
+}
+function kf()
+{
+    document.getElementById('k').style.border='1px solid green';
+}
+function lf()
+{
+    document.getElementById('l').style.border='1px solid green';
+}
+function mf()
+{
+    document.getElementById('m').style.border='1px solid green';
+}
+function nf()
+{
+    document.getElementById('n').style.border='1px solid green';
+}
+function of()
+{
+    document.getElementById('o').style.border='1px solid black';
+}
+function pf()
+{
+    document.getElementById('p').style.border='1px solid black';
+}
+function qf()
+{
+    document.getElementById('q').style.border='1px solid black';
+}
+function rf()
+{
+    document.getElementById('r').style.border='1px solid black';
+}
+function sf()
+{
+    document.getElementById('s').style.border='1px solid black';
+}
+function tf()
+{
+    document.getElementById('t').style.border='1px solid black';
+}
+function uf()
+{
+    document.getElementById('u').style.border='1px solid black';
+}
+function vf()
+{
+    document.getElementById('v').style.border='1px solid green';
+}
+function wf()
+{
+    document.getElementById('w').style.border='1px solid green';
+}
+function xf()
+{
+    document.getElementById('x').style.border='1px solid green';
+}
+function yf()
+{
+    document.getElementById('y').style.border='1px solid green';
+}
+function zf()
+{
+    document.getElementById('z').style.border='1px solid green';
+}
+
+
+// Playing  code
+function aplay()
+{
+    var audio=new Audio('sound/a.mp3')
+    audio.play();
+}
+function bplay()
+{
+    var audio=new Audio('sound/b.mp3')
+    audio.play();
+}
+function cplay()
+{
+    var audio=new Audio('sound/c.mp3')
+    audio.play();
+}
+function dplay()
+{
+    var audio=new Audio('sound/d.mp3')
+    audio.play();
+}
+function eplay()
+{
+    var audio=new Audio('sound/e.mp3')
+    audio.play();
+}
+function fplay()
+{
+    var audio=new Audio('sound/f.mp3')
+    audio.play();
+}
+function gplay()
+{
+    var audio=new Audio('sound/g.mp3')
+    audio.play();
+}
+function hplay()
+{
+    var audio=new Audio('sound/h.mp3')
+    audio.play();
+}
+function iplay()
+{
+    var audio=new Audio('sound/i.mp3')
+    audio.play();
+}
+function jplay()
+{
+    var audio=new Audio('sound/j.mp3')
+    audio.play();
+}
+function kplay()
+{
+    var audio=new Audio('sound/k.mp3')
+    audio.play();
+}
+function lplay()
+{
+    var audio=new Audio('sound/l.mp3')
+    audio.play();
+}
+function mplay()
+{
+    var audio=new Audio('sound/m.mp3')
+    audio.play();
+}
+function nplay()
+{
+    var audio=new Audio('sound/n.mp3')
+    audio.play();
+}
+function oplay()
+{
+    var audio=new Audio('sound/o.mp3')
+    audio.play();
+}
+function pplay()
+{
+    var audio=new Audio('sound/p.mp3')
+    audio.play();
+}
+function qplay()
+{
+    var audio=new Audio('sound/q.mp3')
+    audio.play();
+}
+function rplay()
+{
+    var audio=new Audio('sound/r.mp3')
+    audio.play();
+}
+function splay()
+{
+    var audio=new Audio('sound/s.mp3')
+    audio.play();
+}
+function tplay()
+{
+    var audio=new Audio('sound/t.mp3')
+    audio.play();
+}
+function uplay()
+{
+    var audio=new Audio('sound/u.mp3')
+    audio.play();
+}
+function vplay()
+{
+    var audio=new Audio('sound/v.mp3')
+    audio.play();
+}
+function wplay()
+{
+    var audio=new Audio('sound/w.mp3')
+    audio.play();
+}
+function xplay()
+{
+    var audio=new Audio('sound/x.mp3')
+    audio.play();
+}
+function yplay()
+{
+    var audio=new Audio('sound/y.mp3')
+    audio.play();
+}
+function zplay()
+{
+    var audio=new Audio('sound/z.mp3')
+    audio.play();
+}
